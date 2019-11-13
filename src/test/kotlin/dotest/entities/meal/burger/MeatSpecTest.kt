dbf6ad40e52e0e223c8cf6823ae264706dbca436
Kaue Silva <kaue.silva@MacBook-Pro-de-admin-2.local>
2019-11-13 15:31:41 -0300

    Tests

--- src/test/kotlin/dotest/entities/meal/burger/MeatSpecTest.kt
+++ src/test/kotlin/dotest/entities/meal/burger/MeatSpecTest.kt
@@ -7,13 +7,22 @@ import org.spekframework.spek2.style.specification.describe
 
 class MeatSpecTest : Spek({
     describe("when asking if meat is ready") {
-        describe("and the meat is raw") {
+        describe("and the meat is not ready") {
             it("should return false") {
                 val meat = Meat(ready = false)
 
                 val subject = meat.isReady()
 
                 expect(subject).to.be.`false`
+            }
+        }
+        describe("and the meat is ready") {
+            it("should return true") {
+                val meat = Meat(ready = true)
+
+                val subject = meat.isReady()
+
+                expect(subject).to.be.`true`
             }
         }
     }
